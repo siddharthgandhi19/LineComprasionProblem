db9e0e813fb0bba671ebe5d0635e2230bb54f84f
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-08 13:28:45 +0530

    [Siddharth]Add: Equality Of Two Lines

--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -8,9 +8,14 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to line comparision problem statement ");
-            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
-            calculate.Calculate();
-
+            CalculateLength calculate1 = new CalculateLength(1, 2, 3, 4);
+            double length1 = calculate1.Calculate();
+            CalculateLength calculate2 = new CalculateLength(1, 2, 4, 4);
+            double length2 = calculate2.Calculate();
+            if (length1.Equals(length2))//length1==length2
+                Console.WriteLine("Both Lines are Equals");
+            else
+                Console.WriteLine("Both Lines are Not Equals");
         }
     }
 }

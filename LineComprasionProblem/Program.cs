a989b3c70588112421748e551c821b4490a305d2
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-08 13:24:24 +0530

    [Siddharth]Add: Calculate Length

--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LineComparision;
+using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparision problem statement ");
+            CalculateLength calculate = new CalculateLength(1, 2, 3, 4);
+            calculate.Calculate();
+
+        }
+    }
+}

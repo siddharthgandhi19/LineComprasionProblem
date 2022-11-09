5c13a03514466ba273c4b853c86bfe9350119ee2
Siddharth Gandhi <113658358+siddharthgandhi19@users.noreply.github.com>
2022-11-09 15:22:34 +0530

    Update CalculateLength.cs

--- LineComprasionProblem/CalculateLength.cs
+++ LineComprasionProblem/CalculateLength.cs
@@ -15,6 +15,7 @@ namespace LineComparision
         {
             double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
             Console.WriteLine("Length:-" + length);
+            return length;
         }
     }
 }
